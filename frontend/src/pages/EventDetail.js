13e0543472400a770b1996d01589e5e022116d72
adanglingpointer <dave@imail.cloud>
2024-04-23 19:46:24 -0400

    working with action function

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,10 +1,24 @@
-import { useParams } from "react-router-dom";
+import { json, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useRouteLoaderData("event-detail");
 
-  return <EventItem event="" />;
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fecth details for selected event" },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+}
