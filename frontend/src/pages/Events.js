fbf538ad289724373152302f6bfff502c2330e81
adanglingpointer <dave@imail.cloud>
2024-05-13 20:51:53 -0400

    finishing up

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,14 +1,17 @@
 import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
   const { events } = useLoaderData();
 
   return (
-    <Await resolve={events}>
-      {(loadedEvents) => <EventsList events={loadedEvents} />}
-    </Await>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
@@ -18,20 +21,10 @@ async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events." };
-    // or
-    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-    //   status: 500,
-    // });
     throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    // const resData = await response.json();
-    // return resData.events;
-    //
-    // const res = new Response("any data", { status: 201 });
-    // return res;
-    //
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
 }
 
